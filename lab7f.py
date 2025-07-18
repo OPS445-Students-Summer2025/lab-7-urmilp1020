59738153d8069b2c2bfc3a652a0a42fe0cac69a1
Urmil patel <urpatel5@myseneca.ca>
2025-07-18 10:21:26 -0400

    Added lab7f

---
+++ lab7f.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+class Time:
+    def __init__(self, hour=12, minute=0, second=0):
+        self.hour = hour
+        self.minute = minute
+        self.second = second
+
+    def __str__(self):
+        return f'{self.hour:02d}:{self.minute:02d}:{self.second:02d}'
+
+    def __repr__(self):
+        return f'{self.hour:02d}.{self.minute:02d}.{self.second:02d}'
+
+    def format_time(self):
+        return f'{self.hour:02d}:{self.minute:02d}:{self.second:02d}'
+
+    def sum_times(self, t2):
+        total = self.time_to_sec() + t2.time_to_sec()
+        return sec_to_time(total)
+
+    def __add__(self, t2):
+        return self.sum_times(t2)
+
+    def change_time(self, seconds):
+        new_sec = self.time_to_sec() + seconds
+        new_time = sec_to_time(new_sec)
+        self.hour, self.minute, self.second = new_time.hour, new_time.minute, new_time.second
+
+    def time_to_sec(self):
+        return (self.hour * 3600) + (self.minute * 60) + self.second
+
+    def valid_time(self):
+        return 0 <= self.hour < 24 and 0 <= self.minute < 60 and 0 <= self.second < 60
+
+def sec_to_time(seconds):
+    t = Time()
+    minutes, t.second = divmod(seconds, 60)
+    t.hour, t.minute = divmod(minutes, 60)
+    return t
